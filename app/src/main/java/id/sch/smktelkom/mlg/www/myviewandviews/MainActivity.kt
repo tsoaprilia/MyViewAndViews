a45321e7d4eddd5a93f6037b0b08ed45cd2c2bc2
Aprilia Dwi Cristyana <tsoaprilia@gmail.com>
2020-08-29 06:16:32 +0700

    lima commit
    Signed-off-by: Aprilia Dwi Cristyana <tsoaprilia@gmail.com>

---
+++ app/src/main/java/id/sch/smktelkom/mlg/www/myviewandviews/MainActivity.kt
@@ -0,0 +1,17 @@
+package id.sch.smktelkom.mlg.www.myviewandviews
+
+import android.os.Bundle
+import androidx.appcompat.app.ActionBar
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        if (supportActionBar != null) {
+            (supportActionBar as ActionBar).title = "Google Pixel"
+        }
+    }
+}
